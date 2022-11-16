b7a8b949c2ba95b22ead176163ee474bfe75b311
Aaron Gershman <aegershman@gmail.com>
2022-11-16 15:31:58 -0600

    Handlebars helper test, enable npm test to fail

--- index.test.js
+++ index.test.js
@@ -1,5 +1,7 @@
 const { HtmlValidate } = require('html-validate')
 
+const Handlebars = require("handlebars");
+
 const index = require('./index')
 const resume = require('./resume.json')
 
@@ -16,4 +18,22 @@ describe.skip('render function', () => {
     const validator = new HtmlValidate()
     expect(validator.validateString(index.render(resume)).valid).toBe(true)
   })
+})
+
+
+describe('Handlebars Tests', () => {
+
+  describe('date helpers', () => {
+
+    test('MONTH_YEAR converts month as an integer into abbreviated English format', () => {
+      this.fields = { date: "2013-12-01" };
+      this.html = "{{MONTH_YEAR date}}";
+      var template = Handlebars.compile(this.html);
+      this.fields['date'] = "2013-12-01";
+      var result = template(this.fields);
+      expect(result).toEqual("Nov 2013");
+    })
+
+  })
+
 })
